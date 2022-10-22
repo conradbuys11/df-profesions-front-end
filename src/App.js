e51e91dd5be1d6ee282545b4ab7f395ebd507233
conradbuys11 <conradbuys11@gmail.com>
2022-10-22 19:16:05 -0400

    ready to push to main
    just did some edits to homepage, links to stuff

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import {Route, Routes} from 'react-router-dom';
 
 function App() {
 
-  return ( <Routes>
-    <Route path="/" element={<Homepage />}/>
-    <Route path="/basics" element={<Basics />}/>
-    <Route path="/professions" element={<Professions />} />
-  </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />}/>
+      <Route path="/basics" element={<Basics />}/>
+      <Route path="/professions" element={<Professions />} />
+    </Routes>
   );
     // <div className="App">
     //   <h1>Welcome to the DF Profession Project - Under Construction</h1>
