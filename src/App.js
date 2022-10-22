023da5264595c74115a666b63f88074c2d1f92f5
conradbuys11 <conradbuys11@gmail.com>
2022-10-22 18:55:31 -0400

    added react-router, setting up routes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Homepage from './components/Homepage.js'
 import Basics from './components/Basics.js';
 import Professions from './components/Professions.js';
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
 
-  return (
-    <div className="App">
-      <h1>Welcome to the DF Profession Project - Under Construction</h1>
-      {/* < Homepage /> */}
-      {/* < Basics /> */}
-      <Professions />
-    </div>
+  return ( <Routes>
+    <Route path="/" element={<Homepage />}/>
+    <Route path="/basics" element={<Basics />}/>
+    <Route path="/professions" element={<Professions />} />
+  </Routes>
   );
+    // <div className="App">
+    //   <h1>Welcome to the DF Profession Project - Under Construction</h1>
+    //   {/* < Homepage /> */}
+    //   {/* < Basics /> */}
+    //   <Professions />
+    // </div>
 }
 
 export default App;
