cb6bb3192c9412d84657585ffdb5c04d2e0151de
conradbuys11 <conradbuys11@gmail.com>
2022-11-05 16:46:09 -0400

    tweaked a common method

--- src/Common.js
+++ src/Common.js
@@ -18,8 +18,23 @@ const keyToWords = word => {
             return "EZ-Thro";
         case "Artisans":
             return "Artisan's Consortium"
+        case "Belts":
+            return ktwHelperMethod(words);
+        case "Boots":
+            return ktwHelperMethod(words);
         default:
             return words.join(" ");
+    }
+}
+
+const ktwHelperMethod = words => {
+    //if this is BracersLeather or BootsMail, OR BootsLeather or BootsMail
+    //return Bracers (Leather) or Bracers (Mail)
+    if(words.length > 1){
+        return `${words[0]} (${words[1]})`
+    }
+    else{
+        return words[0];
     }
 }
 
