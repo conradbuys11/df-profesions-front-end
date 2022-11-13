673daea98d49c46b6e3a76c48a5abcaf39a57995
conradbuys11 <conradbuys11@gmail.com>
2022-11-12 21:14:16 -0500

    back to localhost:3001

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import NotFoundPage from "./components/NotFoundPage";
 import { checkFetchError } from "./Common";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const URL = "https://dragonflight-professions-api.onrender.com";
+const URL = "http://localhost:3001";
 
 const fetchAllInfo = (baseURL, setState) => {
   //baseURL is either localhost:3001 or dragonflight-professions-api.onrender.com
