0c16c851d1b86da9d461dcef05269ae226862543
conradbuys11 <conradbuys11@gmail.com>
2022-11-13 23:49:42 -0500

    prof recipes sorting methods
    basically recreating the functionality from the API

--- src/ApiNavigation.js
+++ src/ApiNavigation.js
@@ -5,6 +5,48 @@ const ApiNavigation = (db) => {
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
   // almost all our return types are gonna be either a) arrays (if we're getting multiple) or b) objects (if we're getting one)
 
+  // some explanations:
+
+  // by nesting functions and returning the children functions as an obj at the end of the parent function,
+  // we can specialize methods a whole hell of a lot, and access certain data only when we need it
+
+  // regex: new RegExp(string, letter) will give us a regex with that string, and apply modifications based on letter
+  // i.e. "i" is case insensitive - no having to worry about using .toLowerCase() ad nauseum
+
+  // funky sorting method: return a["fieldName"] - b["fieldName"] || a["otherFieldName"] - b["otherFieldName"]
+  // basically, we sort by the first field. if it's not 1 or -1 (true), that means they're the same
+  // so we can then sort by the second field
+
+  // HELPER METHODS
+  const sortStrings = (stringA, stringB) => {
+    const a = stringA.toUpperCase();
+    const b = stringB.toUpperCase();
+    return a < b ? -1 : a > b ? 1 : 0;
+  };
+
+  const sortObjects = (objA, objB) => {
+    //Object.getOwnPropertyNames returns array of the key names of our obj
+    //only one key in these objects! so we can just get index 0
+    const aName = Object.getOwnPropertyNames(objA)[0];
+    const bName = Object.getOwnPropertyNames(objB)[0];
+    if (aName.toUpperCase() > bName.toUpperCase()) {
+      return 1;
+    }
+    if (aName.toUpperCase() < bName.toUpperCase()) {
+      return -1;
+    }
+
+    // this code will only run if the keys were equal
+    const aValue = objA[aName];
+    const bValue = objB[bName];
+    if (typeof aValue == "number") {
+      return aValue - bValue;
+    }
+
+    //this will only run if the values are strings
+    return sortStrings(aValue, bValue);
+  };
+
   // ITEM METHODS
   const getItem = () => {
     const byId = (id) => {
@@ -27,24 +69,22 @@ const ApiNavigation = (db) => {
     };
 
     const profToolAndAccessories = () => {
-      const byProfessionId = (id) => {
-        const profession = getProfession.byId(id);
-        const regex = new RegExp(
-          `${profession.name} Accessory|${profession.name} Tool`,
-          "i"
-        );
-        return db.items.filter((item) => item.armorWeaponType.match(regex));
-      };
+      const items = db.items.filter((item) =>
+        item.armorWeaponType.match(/Accessory|Tool/i)
+      );
 
       const byProfessionName = (name) => {
-        const regex = new RegExp(`${name} Accessory|${name} Tool`, "i");
-        return db.items.filter((item) => item.armorWeaponType.match(regex));
+        const regex = new RegExp(name, "i");
+        return items.filter((item) => item.armorWeaponType.match(regex));
+      };
+
+      const byProfessionId = (id) => {
+        const professionName = getProfession.byId(id).name;
+        return byProfessionName(professionName);
       };
 
       const all = () => {
-        return db.items.filter((item) =>
-          item.armorWeaponType.match(/Accessory|Tool/i)
-        );
+        return items;
       };
 
       return { byProfessionId, byProfessionName, all };
@@ -68,7 +108,70 @@ const ApiNavigation = (db) => {
 
   const getRecipes = () => {
     const byProfession = (id) => {
-      return db.recipes.filter((recipe) => recipe.professionId === id);
+      const recipes = db.recipes.filter((recipe) => recipe.professionId === id);
+
+      const onlyTrainerRecipes = () => {
+        const trainerRecipes = recipes.filter(
+          (recipe) => recipe.requiredProfessionLevel
+        );
+        trainerRecipes.sort(
+          (a, b) =>
+            a.requiredProfessionLevel - b.requiredProfessionLevel ||
+            sortStrings(a.name, b.name)
+        );
+        return trainerRecipes;
+      };
+
+      const onlyRenownRecipes = () => {
+        const renownRecipes = recipes.filter(
+          (recipe) => recipe.requiredRenownLevel
+        );
+        renownRecipes.sort(
+          (a, b) =>
+            sortObjects(a.requiredRenownLevel, b.requiredRenownLevel) ||
+            sortStrings(a.name, b.name)
+        );
+        return renownRecipes;
+      };
+
+      const onlySpecializationRecipes = () => {
+        const specRecipes = recipes.filter(
+          (recipe) =>
+            recipe.requiredProfessionLevel && !recipe.specialAcquisitionMethod
+        );
+        specRecipes.sort(
+          (a, b) =>
+            sortObjects(a.requiredProfessionLevel, b.requiredProfessionLevel) ||
+            sortStrings(a.name, b.name)
+        );
+        return specRecipes;
+      };
+
+      const onlyOtherRecipes = () => {
+        const otherRecipes = recipes.filter(
+          (recipe) => recipe.specialAcquisitionMethod
+        );
+        otherRecipes.sort(
+          (a, b) =>
+            sortStrings(
+              a.specialAcquisitionMethod,
+              b.specialAcquisitionMethod
+            ) || sortStrings(a.name, b.name)
+        );
+        return otherRecipes;
+      };
+
+      const all = () => {
+        return recipes.sort((a, b) => sortStrings(a.name, b.name));
+      };
+
+      return {
+        onlyTrainerRecipes,
+        onlyRenownRecipes,
+        onlySpecializationRecipes,
+        onlyOtherRecipes,
+        all,
+      };
     };
 
     const byItem = (id) => {
