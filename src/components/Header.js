55edb06a07c628bb928ca1c4259b3c845d77208a
conradbuys11 <conradbuys11@gmail.com>
2022-11-17 13:55:17 -0500

    header dropdown working
    took way too long to figure out why, time to do some responsive designing!

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,24 +6,32 @@ import NavDropdown from "react-bootstrap/esm/NavDropdown";
 import { LinkContainer } from "react-router-bootstrap";
 import HeaderSearchBar from "./HeaderSearchBar";
 import "./Header.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Header = (props) => {
   // props: db, apiNavigation (for our HeaderSearchBar)
   return (
     <div className="Header">
-      <Navbar sticky="top">
+      <Navbar sticky="top" expand="xl">
         <Container>
+          {/* <Row>
+            <Col xs={4}> */}
           <LinkContainer to="/">
             <Navbar.Brand id="header-logo">DF Professions</Navbar.Brand>
           </LinkContainer>
+          {/* </Col>
+            <Col xs={4}> */}
           <Nav className="justify-content-center">
             <HeaderSearchBar
               db={props.db}
               apiNavigation={props.apiNavigation}
             />
           </Nav>
+          {/* </Col>
+            <Col xs={4}> */}
           <Navbar.Toggle aria-controls="navbar-pages" />
-          <Navbar.Collapse id="navbar-pages" className="justify-content-end">
+          <Navbar.Collapse className="justify-content-end" id="navbar-pages">
             <Nav>
               <LinkContainer to="/basics">
                 <Nav.Link className="header-page">Basics</Nav.Link>
@@ -31,11 +39,16 @@ const Header = (props) => {
               <LinkContainer to="/professions">
                 <Nav.Link className="header-page">Profession List</Nav.Link>
               </LinkContainer>
+              <LinkContainer to="/items/finishingreagents">
+                <Nav.Link className="header-page">Finishing Reagents</Nav.Link>
+              </LinkContainer>
               <LinkContainer to="/">
                 <Nav.Link className="header-page">Contact (NYI)</Nav.Link>
               </LinkContainer>
             </Nav>
           </Navbar.Collapse>
+          {/* </Col>
+          </Row> */}
         </Container>
       </Navbar>
     </div>
