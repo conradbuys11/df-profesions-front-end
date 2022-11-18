2b0003dd4ae2b066673bb5fd7e3f47cc127db0df
conradbuys11 <conradbuys11@gmail.com>
2022-11-18 11:10:08 -0500

    quick fix for temp load, deploy inc

--- src/components/TempLoad.js
+++ src/components/TempLoad.js
@@ -1,11 +1,13 @@
 import "./TempLoad.css";
 
 const TempLoad = (props) => {
-  <div className="Temp-Load">
-    <h1 className="header-xl">
-      Hi there! Just waiting for the DB to load! Give me a second!
-    </h1>
-  </div>;
+  return (
+    <div className="Temp-Load">
+      <h1 className="header-xl">
+        Hi there! Just waiting for the DB to load! Give me a second!
+      </h1>
+    </div>
+  );
 };
 
 export default TempLoad;
